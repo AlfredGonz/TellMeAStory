c1c19ee845539528e84ba9a1d287d14bdcd3047a
unknown <manu.amg95@gmail.com>
2018-09-11 19:22:40 -0600

    interfaz de la app

---
+++ app/src/main/java/sv/com/ariel/tellmeastory/Activities/AgregarHistoria.java
@@ -0,0 +1,15 @@
+package sv.com.ariel.tellmeastory.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import sv.com.ariel.tellmeastory.R;
+
+public class AgregarHistoria extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_historia);
+    }
+}
